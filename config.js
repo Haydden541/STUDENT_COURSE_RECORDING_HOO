cd44e86f50f080e5a896a39019d91013cb828056
Haydden541 <1365213202@qq.com>
2026-03-08 23:23:16 +0800

    Add student data to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const studentData = [
+    { name: "张三", major: "交互设计", country: "美国" },
+    { name: "李四", major: "游戏开发", country: "英国" },
+    { name: "Alex", major: "Digital Art", country: "澳大利亚" }
+];
